2c2bfb37819bfc6e407bbceaa891e565c5f1d3a5
Ivar Grimstad <ivar.grimstad@cybercom.com>
2018-04-18 09:38:47 +0200

    Add Birthday Duke

---
+++ birthday-duke/src/main/java/eu/agilejava/birthdayduke/rest/BirthdayEndpoint.java
@@ -0,0 +1,40 @@
+package eu.agilejava.birthdayduke.rest;
+
+import org.eclipse.microprofile.metrics.annotation.Metered;
+
+import javax.enterprise.context.RequestScoped;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Response;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static java.time.LocalDate.parse;
+import static java.time.temporal.ChronoUnit.DAYS;
+
+@Path("birthday")
+@RequestScoped
+public class BirthdayEndpoint {
+
+    @GET
+    @Produces("text/plain")
+    @Metered
+    public Response doGet(@QueryParam("date") String date) {
+
+        return Response.ok("Days left: " + calculateDaysToBirthday(date)).build();
+    }
+
+    private int calculateDaysToBirthday(final String birthDate) {
+
+        final LocalDate now = LocalDate.now();
+        final LocalDate bdThisYear = parse(birthDate, DateTimeFormatter.ISO_DATE).withYear(now.getYear());
+
+        if (bdThisYear.isAfter(now)) {
+            return (int) now.until(bdThisYear, DAYS);
+        } else {
+            return (int) now.until(bdThisYear.plusYears(1), DAYS);
+        }
+    }
+}
